b0dadef1478271193bab9abc071e863c3578a107
michal.dec@codewise.com <michal.dec@codewise.com>
2015-11-13 20:04:36 +0100

    Improvments

--- aws_ami/script.sh
+++ aws_ami/script.sh
@@ -3,8 +3,6 @@
 # Sleep for a short while until the instance fully boots up.
 sleep 15
 
-# Execute all comands as root
-
 # Update packages
 sudo yum -y update
 
@@ -22,9 +20,7 @@ sudo chkconfig docker on
 sudo service docker start
 
 # Login to quay.io account
-sudo docker login -e="michaldec@gmail.com" \ 
-				  -u="michal_dec+deploy" \
-				  -p="" quay.io
+sudo docker login -e="." -u="michal_dec+deploy" -p="1PCLJHPY3CBN1J2CDPY4QK4IWFCBZT270IC6KHILHEOEW044NVI0MHNN815Y651C" quay.io
 
 # Start docker container
-# sudo docker run -d --name springboot_workshops --restart=always -p 8070:8070 quay.io/michal_dec/springboot_workshops:1
+sudo docker run -d --name springboot_workshops --restart=always -p 8070:8070 quay.io/michal_dec/springboot_workshops:1
--- docker_images/base_java/run-app.sh
+++ docker_images/base_java/run-app.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-echo $JAVA_OPTS
-
-java $JAVA_OPTS -jar /opt/application/application.jar
+java -jar /opt/application/application.jar
